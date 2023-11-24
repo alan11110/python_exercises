cecfaeb2df2705456c9f2c01f972740e06c88614
alan11110 <alanfelipe1110@hotmail.com>
2023-11-24 19:05:14 -0300

    ex 93 done

--- ex93_dic_py.py
+++ ex93_dic_py.py
@@ -3,10 +3,27 @@
 
 infos ={}# simbolo dicionario
 gols = []
-infos['Nome'] = str(input('Digite o nome do jogar: '))
-infos['Qtd partidas'] = input(f' Quantas partidas o jogador {infos["Nome"]}: ')
+n_gol = 0
+soma = int() 
+increment = 0
 
-for i in range (int(infos['Qtd partidas'])):
-    gols.append =  input(f'Quantos gols o {infos["Nome"]}  fez na {i+1} partida?: ')
-    
-print(gols)
+infos['Nome'] = str(input('Digite o nome do jogador: '))
+qtd_partida = int(input(f' Quantas partidas o jogador {infos["Nome"]} jogou?  '))
+for i in range (0, qtd_partida):
+    n_gol =int(input(f'Quantos gols o {infos["Nome"]}  fez na {i+1} partida? '))
+    gols.append(n_gol)
+    soma = n_gol + soma
+print('=-'*40)
+infos['Qtd gols por partida'] = gols
+infos['total'] = int(0)
+infos['total'] = soma
+print(infos)
+print('=-'*40)
+for k,v in infos.items():
+    print(f'O campo {k} tem o valor {v}')
+print('=-'*40)
+print(f'O Jogador {infos["Nome"]} jogou {qtd_partida} partidas.')
+for k in range (1, qtd_partida+1):
+    print(f'-> Na partida {k}, fez {infos["Qtd gols por partida"][increment]} gols.')
+    increment += 1
+print(f'Foi um total de {infos["total"]} gols.')
